6313756e413b4ae5ed0c2a5149f0a5bd26ca2914
michaeltorres1 <torres173@connect.wcsu.edu>
2019-09-10 14:47:54 -0700

    Add greetings presentational component, and sesson form

--- frontend/hto_show.jsx
+++ frontend/hto_show.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
+import { login, logout } from './actions/session_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
 
+  window.login = login;
+  window.logout = logout;
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   
