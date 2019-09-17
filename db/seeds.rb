c0a6bcf25cfe9574a0eaf9c1f3d7c0be03ccabcb
michaeltorres1 <torres173@connect.wcsu.edu>
2019-09-16 17:56:31 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Movie.create!(title: "Aquaman", release_date: "9-12-2018", rating: "5", description: "A water hero movie", director_id: 1, producer_id: 2, writer_id: 3, data_added: "9-10-2019")
+
+aquaman = Movie.find_by(title: "Aquaman")
+
+aquaman.photo.attach(io: File.open("/Users/michaeltorres/Desktop/movie_pics/aquaman.jpg"), filename: "aquaman.jpg")
