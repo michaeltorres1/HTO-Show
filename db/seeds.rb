7247e81c6b2a976ffcd64aeb5c3fd442c4b4730f
michaeltorres1 <torres173@connect.wcsu.edu>
2019-09-16 18:00:34 -0700

    Changed data_added to date_added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Movie.create!(title: "Aquaman", release_date: "9-12-2018", rating: "5", description: "A water hero movie", director_id: 1, producer_id: 2, writer_id: 3, data_added: "9-10-2019")
+Movie.create!(title: "Aquaman", release_date: "9-12-2018", rating: "5", description: "A water hero movie", director_id: 1, producer_id: 2, writer_id: 3, date_added: "9-10-2019")
 
 aquaman = Movie.find_by(title: "Aquaman")
 
