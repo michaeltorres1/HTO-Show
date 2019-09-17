cdd832b55add90b6617bf344d8a2554475562fe3
michaeltorres1 <torres173@connect.wcsu.edu>
2019-09-16 18:12:23 -0700

    Add destroy_all

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Movie.destroy_all
+
 Movie.create!(title: "Aquaman", release_date: "9-12-2018", rating: "5", description: "A water hero movie", director_id: 1, producer_id: 2, writer_id: 3, date_added: "9-10-2019")
 
 aquaman = Movie.find_by(title: "Aquaman")
